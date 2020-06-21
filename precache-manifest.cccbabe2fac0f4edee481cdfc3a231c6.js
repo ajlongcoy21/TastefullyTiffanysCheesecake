83cc21a342adefafb6a47fa44b2f829e7fdb7d8d
ajlongcoy21 <ajlongcoy21@gmail.com>
2020-06-21 15:11:12 -0500

    Updates

---
+++ precache-manifest.cccbabe2fac0f4edee481cdfc3a231c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5bfc2943ac3d1a25fc4db9a4b9a4908",
+    "url": "/TastefullyTiffanysCheesecakes/index.html"
+  },
+  {
+    "revision": "8473c8f3a1045a3c3be3",
+    "url": "/TastefullyTiffanysCheesecakes/static/css/main.a3b384a8.chunk.css"
+  },
+  {
+    "revision": "aebae2e9c649625600cd",
+    "url": "/TastefullyTiffanysCheesecakes/static/js/2.c1696255.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TastefullyTiffanysCheesecakes/static/js/2.c1696255.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8473c8f3a1045a3c3be3",
+    "url": "/TastefullyTiffanysCheesecakes/static/js/main.1339dab9.chunk.js"
+  },
+  {
+    "revision": "27e0a91a7235067ccdf3",
+    "url": "/TastefullyTiffanysCheesecakes/static/js/runtime-main.0983450c.js"
+  }
+]);
