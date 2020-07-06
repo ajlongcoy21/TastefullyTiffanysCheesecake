a09f4a68eddfb856c71dc2ebe1de106f0a9f18dd
ajlongcoy21 <ajlongcoy21@gmail.com>
2020-07-05 22:35:18 -0500

    Updates

---
+++ precache-manifest.ff65e4d5bdd2c4dda5cc9e8eb4cb198d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d550b5e7ceab933e44c7ffcfab29f7e",
+    "url": "/TastefullyTiffanysCheesecakes/index.html"
+  },
+  {
+    "revision": "3a9666234bf4a3c8fe6d",
+    "url": "/TastefullyTiffanysCheesecakes/static/css/2.a1b7a17e.chunk.css"
+  },
+  {
+    "revision": "c3244e74b6445abe30bb",
+    "url": "/TastefullyTiffanysCheesecakes/static/css/main.cfc4db36.chunk.css"
+  },
+  {
+    "revision": "3a9666234bf4a3c8fe6d",
+    "url": "/TastefullyTiffanysCheesecakes/static/js/2.ca85014c.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/TastefullyTiffanysCheesecakes/static/js/2.ca85014c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3244e74b6445abe30bb",
+    "url": "/TastefullyTiffanysCheesecakes/static/js/main.bb70eca2.chunk.js"
+  },
+  {
+    "revision": "27e0a91a7235067ccdf3",
+    "url": "/TastefullyTiffanysCheesecakes/static/js/runtime-main.0983450c.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/TastefullyTiffanysCheesecakes/precache-manifest.ff65e4d5bdd2c4dda5cc9e8eb4cb198d.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/TastefullyTiffanysCheesecakes/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
